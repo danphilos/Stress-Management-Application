9e71faae56122d5dfe86cf28bce8d34de692bd1a
danphilos <ogoladaniel21@gmail.com>
2023-08-23 18:21:47 +0300

    Updated main.dart

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:stress_management_app/splashscreen.dart';
 import 'package:page_transition/page_transition.dart';
+//import 'package:page_transition/page_transition.dart';
 
 void main() {
   runApp(MyApp());
